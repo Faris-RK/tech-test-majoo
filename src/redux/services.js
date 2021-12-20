2230b17221ee6fae064b54c19e37b339580a1fc2
Faris Rahadian <farisrahadian26@gmail.com>
2021-12-21 00:06:56 +0700

    clean up code

--- src/redux/services.js
+++ src/redux/services.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+// import axios from "axios";
 
-// axios.get('https://virtserver.swaggerhub.com/hanabyan/todo/1.0.0/to-do-list')
-// .then((response) => {
-//   console.log(response)
-// })
-const API_URL =
-  "https://virtserver.swaggerhub.com/hanabyan/todo/1.0.0/to-do-list";
+// // axios.get('https://virtserver.swaggerhub.com/hanabyan/todo/1.0.0/to-do-list')
+// // .then((response) => {
+// //   console.log(response)
+// // })
+// const API_URL =
+//   "https://virtserver.swaggerhub.com/hanabyan/todo/1.0.0/to-do-list";
 
-export const todoList = () => {
-  return axios.get(API_URL);
-};
-console.log(todoList);
+// export const todoList = () => {
+//   return axios.get(API_URL);
+// };
+// console.log(todoList);
